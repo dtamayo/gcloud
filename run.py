8e06ad30feb5e1944bd89c23fb4778bcbf543532
Daniel Tamayo <tamayo.daniel@gmail.com>
2021-08-14 00:04:52 -0400

    do rebound checks separately

--- run.py
+++ run.py
@@ -32,8 +32,6 @@ def main():
     for root, dirs, files in os.walk('{0}/'.format(bucketname)):
         for file in files:
             if 'run' in file:
-                run_num = file[3:7]
-                
                 sa = rebound.SimulationArchive('{0}/{1}'.format(bucketname, file))
                 sim = sa[0]
                 P0 = sim.particles[1].P
@@ -41,22 +39,28 @@ def main():
                 if sim.t/P0 > orbmax or sim._status == 7: # exceeded max # of orbits or status=7 means had a collision this dt
                     print('Binary {0} finished'.format(file))
                     call("gsutil mv gs://{0}/data/unfinished/{1} gs://{0}/data/finished/".format(bucketname, file), shell=True)
-                else:
-                    with open("submit-job", "w") as of:
-                        of.write("#!/bin/bash -l\n")
-                        of.write("executable\t\t\t\t= {0}.sh\n".format(bucketname))
-                        of.write("arguments\t\t\t\t= {0}\n".format(file))
-                        of.write("transfer_input_files\t= continue_sim.py, {0}.sh, {0}/{1}\n".format(bucketname, file))
-                        of.write("should_transfer_files\t= IF_NEEDED\n")
-                        of.write('Transfer_Output_Files\t= ""\n')
-                        of.write("when_to_transfer_output\t= ON_EXIT\n")
-                        of.write("log\t\t\t\t\t\t= {0}/run.{1}.log\n".format(bucketname, run_num))
-                        of.write("Error\t\t\t\t\t= {0}/err.{1}\n".format(bucketname, run_num))
-                        of.write("Output\t\t\t\t\t= {0}/out.{1}\n".format(bucketname, run_num))
-                        of.write("queue")
+                    call("gsutil rm {0}/{1}".format(bucketname, file)
 
-                    call("condor_submit submit-job", shell=True)
-                    call("gsutil mv gs://{0}/data/unfinished/{1} gs://{0}/data/queued/".format(bucketname, file), shell=True)
+    for root, dirs, files in os.walk('{0}/'.format(bucketname)):
+        for file in files:
+            if 'run' in file:
+                run_num = file[3:7]
+                
+                with open("submit-job", "w") as of:
+                    of.write("#!/bin/bash -l\n")
+                    of.write("executable\t\t\t\t= {0}.sh\n".format(bucketname))
+                    of.write("arguments\t\t\t\t= {0}\n".format(file))
+                    of.write("transfer_input_files\t= continue_sim.py, {0}.sh, {0}/{1}\n".format(bucketname, file))
+                    of.write("should_transfer_files\t= IF_NEEDED\n")
+                    of.write('Transfer_Output_Files\t= ""\n')
+                    of.write("when_to_transfer_output\t= ON_EXIT\n")
+                    of.write("log\t\t\t\t\t\t= {0}/run.{1}.log\n".format(bucketname, run_num))
+                    of.write("Error\t\t\t\t\t= {0}/err.{1}\n".format(bucketname, run_num))
+                    of.write("Output\t\t\t\t\t= {0}/out.{1}\n".format(bucketname, run_num))
+                    of.write("queue")
+
+                call("condor_submit submit-job", shell=True)
+                call("gsutil mv gs://{0}/data/unfinished/{1} gs://{0}/data/queued/".format(bucketname, file), shell=True)
 
 if __name__ == '__main__':
     main()
